62006b2fa1ab45a06e5dcc453c8a4e29d14f22e8
AilenOne <meiyouyouxiang007@gmail.com>
2018-11-22 15:47:03 +0800

    Create BuilderPattern.cpp

---
+++ Creational Patterns/BuilderPattern.cpp
@@ -0,0 +1,56 @@
+
+
+
+
+
+class IBuilder
+{
+	virtual void builderPart1();
+	virtual void builderPart2();
+	virtual void builderPart3();
+};
+
+class Idirector
+{
+public:
+	virtual void setBuilder(IBuilder& builder)
+	{
+		itsBuilder = &builder;
+	}
+
+	virtual void construct() = 0;
+
+protected:
+	IBuilder* itsBuilder;
+};
+
+class Director : public Idirector
+{
+public:
+	void construct()
+	{
+		itsBuilder->builderPart1();
+		itsBuilder->builderPart2();
+		itsBuilder->builderPart2();
+		itsBuilder->builderPart3();
+	}
+};
+
+
+class OrderBuilder : public IBuilder
+{
+	void builderPart1();
+	void builderPart2();
+	void builderPart3();
+};
+
+
+int test()
+{
+	OrderBuilder builder;
+	Director director;
+	director.setBuilder(builder);
+	director.construct();
+}
+
+
