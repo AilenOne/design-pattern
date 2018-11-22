7c2c992f0d0ee86542b1b77c674a4de23dc463cd
AilenOne <meiyouyouxiang007@gmail.com>
2018-11-22 15:01:44 +0800

    Create FactoryPattern.cpp
    Add new FactoryPattern.cpp

---
+++ Creational Patterns/FactoryPattern.cpp
@@ -0,0 +1,69 @@
+
+
+class IClient
+{
+public:
+	virtual const std::string getClientName() = 0;
+};
+
+class ClientA : public IClient
+{
+public:
+	ClientA()
+	{cout << "create ClientA!" << endl; }
+
+	const std::string getClientName()
+	{
+		return "ClientA";
+	}
+};
+
+class ClientB : public IClient
+{
+public:
+	ClientB()
+	{cout << "create ClientB!" << endl; }
+
+	const std::string getClientName()
+	{
+		return "ClientB";
+	}
+};
+
+class AbstracetFactory
+{
+public:
+	virtual IClient* createProduct() = 0;
+};
+
+class FactoryA : public AbstracetFactory
+{
+public:
+	IClient* createProduct()
+	{
+		return new ClientA;
+	}
+};
+
+
+class FactoryB : public AbstracetFactory
+{
+public:
+	IClient* createProduct()
+	{
+		return new ClientB;
+	}
+};
+
+
+int test() {
+	AbstracetFactory* FactoryA = new  FactoryA();
+	IClient* clientA =  FactoryA->createProduct();
+  clientA->getClientName();
+  
+  
+  AbstracetFactory* FactoryB = new  FactoryB();
+	IClient* clientB =  FactoryB->createProduct();
+	clientB->getClientName();
+	return 0;
+}
