480a0ee12ddb2e6c53691f6bb5f79a280f07880e
AilenOne <meiyouyouxiang007@gmail.com>
2018-11-23 14:31:49 +0800

    Update Mediator.cpp

--- BehavioralPatterns/Mediator.cpp
+++ BehavioralPatterns/Mediator.cpp
@@ -1,23 +1,23 @@
 
 
-class IColleague
+class ICustomer
 {
 public:
-	IColleague();
+	ICustomer();
 	virtual void sendMessage(const IMediator& mediator, const std::string& message) = 0;
-	virtual void receiveMessage(const std::string& message, IColleague* colleague) = 0;
+	virtual void receiveMessage(const std::string& message, ICustomer* customer) = 0;
 };
 
-class ConcrateColleague : public IColleague
+class ConcrateCustomer : public ICustomer
 {
 public:
-	ConcrateColleague();
+	ConcrateCustomer();
 	void sendMessageMessage(const IMediator& mediator, const std::string& message)
 	{
 		mediator.send(message, this);
 	}
 
-	void receiveMessage(const std::string& message, IColleague* colleague)
+	void receiveMessage(const std::string& message, ICustomer* customer)
 	{
 		std::cout << "receive message:" << message << std::endl;
 	}
@@ -26,25 +26,25 @@ public:
 class IMediator
 {
 public:
-	virtual void reigsterColleague(IColleague* colleague)
-	{colleagues.emplace_back(colleague);}
+	virtual void reigsterCustomer(ICustomer* customer)
+	{customers.emplace_back(customer);}
 
-	virtual void send(const std::string& message, IColleague* colleague) = 0;
+	virtual void send(const std::string& message, ICustomer* customer) = 0;
 
 protected:
-	std::list<IColleague*>  colleagues;
+	std::list<ICustomer*>  customers;
 };
 
 class ConcreteMediator: public IMediator
 {
 public:
-	virtual void send(const std::string& message, IColleague* senderColleague)
+	virtual void send(const std::string& message, ICustomer* sendercustomer)
 	{
-		for (auto colleague : colleagues)
+		for (auto customer : customers)
 		{
-			if(colleague != senderColleague)
+			if(customer != sendercustomer)
 			{
-				colleague->receiveMessage(message, colleague);
+				customer->receiveMessage(message, sendercustomer);
 			}
 		}
 	}
@@ -53,12 +53,12 @@ public:
 void Test()
 {
 	ConcreteMediator mediator;
-	ConcrateColleague c1;
-	ConcrateColleague c2;
+	ConcrateCustomer c1;
+	ConcrateCustomer c2;
 
 
-	mediator.reigsterColleague(&c1);
-	mediator.reigsterColleague(&c2);
+	mediator.reigsterCustomer(&c1);
+	mediator.reigsterCustomer(&c2);
 
 	c1.sendMessage(mediator, "test message");
 	c2.sendMessage(mediator, "test message");
